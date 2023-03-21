a32a03c24e05d7ab4f98391f5072cab33c930664
Enes <seyit7enes@gmail.com>
2023-03-21 15:29:46 +0100

    comit

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ async function getWeatherData(loc) {
     )} °`;
 
     const windSpeed = data.wind.speed;
-    windSpeedOutput.textContent = windSpeed;
+    windSpeedOutput.textContent = `Wind Speed: ${windSpeed} km/h`;
 
     const sunrise = new Date(data.sys.sunrise * 1000);
     const sunset = new Date(data.sys.sunset * 1000);
@@ -43,17 +43,16 @@ async function getWeatherData(loc) {
     sunsetOutput.textContent = `${sunset.getHours()} : ${sunset.getMinutes()}`;
 
     const pressure = data.main.pressure;
-    pressureOutput.textContent = pressure + "ph";
+    pressureOutput.textContent = `Pressure: ${pressure} ph`;
 
     const humidity = data.main.humidity;
-    humidityOutput.textContent = humidity + "%";
+    humidityOutput.textContent = `Humidity: ${humidity} %`;
 
-    const visibility = Math.ceil(data.visibility / 1000) + "km";
-    visibilityOutput.textContent = visibility;
+    const visibility = Math.ceil(data.visibility / 1000);
+    visibilityOutput.textContent = `Visibility: ${visibility} km `;
 
     const lastUpdate = new Date(data.dt * 1000);
-    lastUpdateOutput.textContent =
-      lastUpdate.getHours() + " : " + lastUpdate.getMinutes();
+    lastUpdateOutput.textContent = `Last Update: ${lastUpdate.getHours()} : ${lastUpdate.getMinutes()}`;
 
     function capitalizeFirstLetter(string) {
       return string.charAt(0).toUpperCase() + string.slice(1);
@@ -91,62 +90,110 @@ async function getWeatherForcast(loc) {
     const response = await fetch(locationUrl);
     if (!response.ok) throw new Error("Newtork Error");
     const data = await response.json();
+
+    //
+    //
+    //
+    //
+    //
     //
     (function getTomorrowWeather() {
+      console.log(data.list[7]);
+      //display the name of the day
       const dayOutput1 = document.getElementById("tomorrow");
+      const dayName = new Date(data.list[7]["dt_txt"]).toLocaleString("en-US", {
+        weekday: "long",
+      });
+      dayOutput1.textContent = dayName;
+      //
+      //display weather condition
       const weatherCondition = Math.ceil(data.list[7].main.temp - 273.15);
       const weatherConditionOutput =
         document.getElementById("weatherCondition");
-      //
-      dayOutput1.textContent = new Date(data.list[7]["dt_txt"]).toLocaleString(
-        "en-US",
-        {
-          weekday: "long",
-        }
-      );
       weatherConditionOutput.textContent = weatherCondition + "°";
+
+      // display cloud status
+      const clouds1Output = document.getElementById("cloudsTomorrow");
+      const cloudsInfo = data.list[7].weather[0]["description"];
+      clouds1Output.textContent = cloudsInfo;
+
+      //display wind speed
+      const windSpeed1 = data.list[7].wind.speed;
+      const windSpeed1Output = document.getElementById("windSpeedTomorrow");
+      windSpeed1Output.textContent = `Wind Speed ${windSpeed1} km/h`;
     })();
     (function getTomorrowWeather2() {
       const dayOutput2 = document.getElementById("tomorrow2");
+      const dayName2 = new Date(data.list[15]["dt_txt"]).toLocaleString(
+        "en-US",
+        {
+          weekday: "long",
+        }
+      );
+      dayOutput2.textContent = dayName2;
+      //
       const weatherCondition2 = Math.ceil(data.list[15].main.temp - 273.15);
       const weatherConditionOutput2 =
         document.getElementById("weatherCondition2");
-      //
-      dayOutput2.textContent = new Date(data.list[15]["dt_txt"]).toLocaleString(
-        "en-US",
-        {
-          weekday: "long",
-        }
-      );
       weatherConditionOutput2.textContent = weatherCondition2 + "°";
+
+      //
+      const clouds2Output = document.getElementById("cloudsTomorrow2");
+      const cloudsInfo2 = data.list[15].weather[0]["description"];
+      clouds2Output.textContent = cloudsInfo2;
+      //
+      const windSpeed2 = data.list[15].wind.speed;
+      const windSpeed2Output = document.getElementById("windSpeedTomorrow2");
+      windSpeed2Output.textContent = `Wind Speed ${windSpeed2} km/h`;
+      //
     })();
     (function getTomorrowWeather3() {
       const dayOutput3 = document.getElementById("tomorrow3");
+      const dayName3 = new Date(data.list[23]["dt_txt"]).toLocaleString(
+        "en-US",
+        {
+          weekday: "long",
+        }
+      );
+      dayOutput3.textContent = dayName3;
+      //
       const weatherCondition3 = Math.ceil(data.list[23].main.temp - 273.15);
       const weatherConditionOutput3 =
         document.getElementById("weatherCondition3");
-      //
-      dayOutput3.textContent = new Date(data.list[23]["dt_txt"]).toLocaleString(
-        "en-US",
-        {
-          weekday: "long",
-        }
-      );
       weatherConditionOutput3.textContent = weatherCondition3 + "°";
+
+      //
+      const clouds3Output = document.getElementById("cloudsTomorrow3");
+      const cloudsInfo3 = data.list[23].weather[0]["description"];
+      clouds3Output.textContent = cloudsInfo3;
+      //
+      const windSpeed3 = data.list[23].wind.speed;
+      const windSpeed3Output = document.getElementById("windSpeedTomorrow3");
+      windSpeed3Output.textContent = `Wind Speed ${windSpeed3} km/h`;
     })();
     (function getTomorrowWeather4() {
       const dayOutput4 = document.getElementById("tomorrow4");
-      const weatherCondition4 = Math.ceil(data.list[31].main.temp - 273.15);
-      const weatherConditionOutput4 =
-        document.getElementById("weatherCondition4");
-      //
-      dayOutput4.textContent = new Date(data.list[31]["dt_txt"]).toLocaleString(
+      const dayName4 = new Date(data.list[31]["dt_txt"]).toLocaleString(
         "en-US",
         {
           weekday: "long",
         }
       );
+      dayOutput4.textContent = dayName4;
+      //
+      const weatherCondition4 = Math.ceil(data.list[31].main.temp - 273.15);
+      const weatherConditionOutput4 =
+        document.getElementById("weatherCondition4");
       weatherConditionOutput4.textContent = weatherCondition4 + "°";
+
+      //
+      const clouds4Output = document.getElementById("cloudsTomorrow4");
+      const cloudsInfo4 = data.list[31].weather[0]["description"];
+      clouds4Output.textContent = cloudsInfo4;
+      //
+      const windSpeed4 = data.list[31].wind.speed;
+      const windSpeed4Output = document.getElementById("windSpeedTomorrow4");
+      windSpeed4Output.textContent = `Wind Speed ${windSpeed4} km/h`;
     })();
     //
   } catch (error) {
@@ -159,3 +206,5 @@ searchLocation.addEventListener("keydown", (e) => {
     getWeatherForcast(searchLocation.value);
   }
 });
+getWeatherData("Kyoto");
+getWeatherForcast("Kyoto");
